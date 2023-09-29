9ab042bed8cc2007804aa28bb9ca0f7edcaefcbb
Skitbet <54096334+Skitbet@users.noreply.github.com>
2023-09-28 21:02:48 -0400

    Fixed indenting

--- Program.cs
+++ Program.cs
@@ -24,56 +24,46 @@
 
         } while (validMode == false);
 
-switch (mode)
-{
-    case 1:
-        Console.WriteLine("Enter file to encrypt:");
-        string? file;
-        do
+        switch (mode)
         {
-            file = Console.ReadLine();
-        } while (file == null);
+            case 1:
+                Console.WriteLine("Enter file to encrypt:");
+                string? file;
+                do
+                {
+                    file = Console.ReadLine();
+                } while (file == null);
 
-        Console.WriteLine("Enter output file:");
-        string? outputFile;
-        do
-        {
-            outputFile = Console.ReadLine();
-        } while (outputFile == null);
+                Console.WriteLine("Enter output file:");
+                string? outputFile;
+                do
+                {
+                    outputFile = Console.ReadLine();
+                } while (outputFile == null);
 
-        encryptor.EncryptFile(file, outputFile);
-        Console.WriteLine("File encrypted successfully.");
-        break;
+                encryptor.EncryptFile(file, outputFile);
+                Console.WriteLine("File encrypted successfully.");
+                break;
 
-    case 2:
-        Console.WriteLine("Enter file to decrypt:");
-        string? decryptFile;
-        do
-        {
-            decryptFile = Console.ReadLine();
-        } while (decryptFile == null); // Change the condition to exit when decryptFile is not null
+            case 2:
+                Console.WriteLine("Enter file to decrypt:");
+                string? decryptFile;
+                do
+                {
+                    decryptFile = Console.ReadLine();
+                } while (decryptFile == null);
 
-        Console.WriteLine("Enter output file:");
-        string? outputDecryptFile;
-        do
-        {
-            outputDecryptFile = Console.ReadLine();
-        } while (outputDecryptFile == null); // Change the condition to exit when outputDecryptFile is not null
+                Console.WriteLine("Enter output file:");
+                string? outputDecryptFile;
+                do
+                {
+                    outputDecryptFile = Console.ReadLine();
+                } while (outputDecryptFile == null);
 
-        encryptor.DecryptFile(decryptFile, outputDecryptFile);
-        Console.WriteLine("File decrypted successfully.");
-        break;
-}
-
-        // // Encrypt the file
-        // encryptor.EncryptFile(inputFile, encryptedFile);
-
-        // Console.WriteLine("File encrypted successfully.");
-
-        // // Decrypt the file
-        // encryptor.DecryptFile(encryptedFile, decryptedFile);
-
-        // Console.WriteLine("File decrypted successfully.");
+                encryptor.DecryptFile(decryptFile, outputDecryptFile);
+                Console.WriteLine("File decrypted successfully.");
+                break;
+        }
     }
 
 }
